1c8c0bdf689eb7135b99cbef44aaf94d6d667a9f
Gustavo Galvis <ggalvis216@gmail.com>
2023-01-13 21:37:45 -0500

    Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,4 +17,31 @@ fun main(args: Array<String>) {
 
     println(PI)
 
+    //val boolean: Boolean = true
+    val boolean = true;
+
+    //val longNumber: Long = 3L
+    val longNumber = 3L
+
+    //val doubleNumber: Double = 2.1245
+    val doubleNumber = 2.1245
+
+    //val floatNumber: Float = 1.1f
+    val floatNumber = 1.1f
+
+    val firstValue = 20
+    val secondValue = 10
+
+    //val thirdValue = firstValue.minus(secondValue)
+    val thirdValue = firstValue - secondValue
+
+    println(thirdValue)
+
+    val firstname = "Gustavo"
+    val lastname = "Galvis"
+    //val fullname = firstname + " " + lastname
+    val fullname = "$firstname $lastname"
+
+    println(fullname)
+
 }
