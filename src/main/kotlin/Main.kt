ea25b0a9fd4839ee0160ab96a2163544e271989a
Gustavo Galvis <ggalvis216@gmail.com>
2023-01-13 21:26:27 -0500

    Creacion proyecto

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+// Constant
+const val PI = 3.1416
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Variable - Write and read
+    //var money: Int = 10
+    var money = 10
+    println(money)
+    money = 5
+    println(money)
+
+    // Variable - Read only
+    val name = "Anna"
+    println(name)
+
+    println(PI)
+
+}
